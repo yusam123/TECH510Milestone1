055a73efbb026a9c2aeff3b018799cac3182e3bf
yusam123 <yusam12tw48@gmail.com>
2023-02-07 23:03:29 -0800

    second

--- static/js/index_page.js
+++ static/js/index_page.js
@@ -7,13 +7,13 @@ function showMessage1() {
  function showMessage2() {
   var x = document.getElementById("age_question");
   var name = document.getElementById("input").value;
-  document.getElementById("age_label").innerHTML = "Hello, " + name + ". How old are you?";
+  document.getElementById("age_label").innerHTML = "Hello, " + name + ". How old are you(please enter only number)?";
   x.style.display = "block";
 }
 function showMessage3() {
   var x = document.getElementById("time_question");
   var name = document.getElementById("input").value;
-  document.getElementById("time_label").innerHTML = "Hello, " + name + ". when did you get up today?";
+  document.getElementById("time_label").innerHTML = "Hello, " + name + ". When did you get up today(please enter only number, for example: 8)?";
   x.style.display = "block";
 }
  
